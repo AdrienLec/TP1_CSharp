463b0b917adab752925e3456beb91ed8d5dfefda
AdrienLec <adrienlecaplain@gmail.com>
2021-02-05 11:32:13 +0100

    Update MyBdContext.cs

--- TP1 2/Database/MyBdContext.cs
+++ TP1 2/Database/MyBdContext.cs
@@ -1,10 +1,37 @@
-﻿using System;
+using System;
+using System.Data.Entity;
+using TP1.Entities;
+
 namespace TP1.Database
 {
-    public class MyBdContext
+    public class MyBdContext : DbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Editor> Editors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookShop> BookShops { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Buy> Buys { get; set; }
+
         public MyBdContext()
         {
+            if (this.Database.CreateIfNotExists())
+            {
+                for (int i =0; i < 2; i++)
+                {
+                    this.Authors.Add(new Entities.Author() { Firstname = "F" + i, Lastname = "L" + i });
+                }
+
+                for (int i = 0; i < 1; i++)
+                {
+                    this.Editors.Add(new Entities.Editor() { Firstname = "F" + i, Lastname = "L" + i });
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    this.Books.Add(new Entities.Book() { Name = "N" + i, Nb_page = "N" + i});
+                }
+            }
         }
     }
 }
